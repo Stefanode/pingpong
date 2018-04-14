0c0ade7233229aa095c0367d65118a80052fe8b0
Steef <stefanodegenkamp@gmail.com>
2018-04-14 15:12:23 +0200

    Logic build based on the 2 rules

--- pingpong.php
+++ pingpong.php
@@ -2,4 +2,26 @@
 $player1 = 0;
 $player2 = 0;
 $round = 0;
+
+// player must reach score of 11
+// player must be a minimum of 2 points higher than opponent
+
+while (abs($player1 - $player2) < 2 || ($player1 < 11 && $player2 < 11)) {
+  $round++;
+  echo "<h2>Round $round</h2>\n";
+  if (rand(0,1)) {
+    $player1++;
+  } else {
+    $player2++;
+  }
+  echo "Player1 = $player1<br />\n";
+  echo "Player2 = $player2<br />\n";
+}
+echo "<h1>";
+if ($player1 > $player2) {
+  echo "Player 1";
+} else {
+  echo "Player 2";
+}
+echo " is the winner after $round rounds!</h1>\n";
 ?>
